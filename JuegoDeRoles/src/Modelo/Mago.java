a534f533dab23650930647b84ea0996b21043264
Dario-455 <antoniodario41@gmail.com>
2026-06-18 17:14:32 -0500

    Dario: agrega clase padre: personaje y clase hija: Guerrero

---
+++ JuegoDeRoles/src/Modelo/Mago.java
@@ -0,0 +1,68 @@
+
+package Modelo;
+
+/**
+ * Representa un personaje de tipo Mago.
+ * El mago utiliza su poder mágico para atacar  y el maná para fortalecer su defensa.
+ *
+ * @author Emily
+ */
+public class Mago extends Personaje {
+
+
+    private static final int BONUS_ATAQUE = 6;
+    private static final int BONUS_DEFENSA = 3;
+
+
+    private int poderMagico;
+    private int mana;
+
+    public Mago(String nombre,int puntosVida,int nivelExperiencia, int poderMagico, int mana) {
+        super(nombre, puntosVida, nivelExperiencia);
+        this.poderMagico = poderMagico;
+        this.mana = mana;
+    }
+
+    @Override
+    public int atacar() {//Calcula el daño producido por el mago.
+        return poderMagico + (nivelExperiencia * BONUS_ATAQUE);
+    }
+
+    @Override
+    public int defender() {//Calcula la defensa del mago.
+        return mana + (nivelExperiencia * BONUS_DEFENSA);
+
+    }
+
+    public int getPoderMagico() {
+        return poderMagico;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public void setPoderMagico(int poderMagico) {
+        if (poderMagico > 0) {
+            this.poderMagico = poderMagico;
+        }
+    }
+
+    public void setMana(int mana) {
+        if (mana >= 0) {
+            this.mana = mana;
+        }
+    }
+
+    @Override
+    public String toString() {
+
+        return "----- MAGO -----"
+                + "\n"
+                + super.toString()
+                + "\nPoder mágico: " + poderMagico
+                + "\nManá: " + mana;
+
+    }
+
+}
